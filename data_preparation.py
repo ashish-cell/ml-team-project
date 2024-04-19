bc537d68b9de0b7b648a2da0a1291c66407575e7
unknown <vatsalmtailor@gmail.com>
2024-04-19 11:36:38 -0500

    vatsal ml flow run

--- data_preparation.py
+++ data_preparation.py
@@ -1,14 +1,9 @@
-# Filename: data_preparation.py
-# This script loads the Boston housing data, performs cleaning, adds new features, and saves the processed data.
 import pandas as pd
-from sklearn.datasets import load_boston
 import numpy as np
 
 def prepare_data():
     # Load data
-    
     data = pd.read_csv("BostonHousing.csv")
-    
 
     # Feature engineering
     data['LSTAT_SQ'] = data['LSTAT'] ** 2
